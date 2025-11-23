b467323870a7ef01f9c745a95b428e7c2338982b
huangbinapple <huangbinapple@gmail.com>
2025-11-23 12:25:53 +0800

    Lock portrait UI and normalize photo orientation

--- Camera/CameraApp.swift
+++ Camera/CameraApp.swift
@@ -7,11 +7,33 @@
 
 import SwiftUI
 
+final class PortraitHostingController<Content: View>: UIHostingController<Content> {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        .portrait
+    }
+
+    override var shouldAutorotate: Bool {
+        false
+    }
+}
+
+private struct PortraitRootView<Content: View>: UIViewControllerRepresentable {
+    let content: Content
+
+    func makeUIViewController(context: Context) -> PortraitHostingController<Content> {
+        PortraitHostingController(rootView: content)
+    }
+
+    func updateUIViewController(_ uiViewController: PortraitHostingController<Content>, context: Context) {
+        uiViewController.rootView = content
+    }
+}
+
 @main
 struct CameraApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PortraitRootView(content: ContentView())
         }
     }
 }
